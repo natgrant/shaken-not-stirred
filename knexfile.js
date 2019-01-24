c5d8d1fd77679dbb7a772a754fe4da106cd717e5
student <mukherjeei@outlook.co.nz>
2019-01-25 09:41:50 +1300

    updating knexfile typo server now running

--- knexfile.js
+++ knexfile.js
@@ -32,12 +32,11 @@ module.exports = {
   //     user: 'username',
   //     password: 'password'
   //   },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
   }
 }
