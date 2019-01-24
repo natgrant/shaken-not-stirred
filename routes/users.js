cd2b57dd715b7eff407fc1c4ecfff0a875a2cde8
student <mukherjeei@outlook.co.nz>
2019-01-25 10:42:49 +1300

    route for list updated

--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,21 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.render("home");
 });
+
+router.get("/list", (req, res) => {
+  res.render("drinksList", { layout: "show" })
+})
+
+
 // .catch(err => {
 //   res.status(500).send("DATABASE ERROR: " + err.message);
 // });
+
+
+//TODO: drinks list route
+
+//TODO: result route
+
+//Edit route??
 
 module.exports = router;
